175f226883e4072779b63be88a1e000905516dd6
Sqh3rd <moitz417@gmail.com>
2026-03-17 20:23:07 +0100

    what's even going one

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 
 PACKAGES=(
-  git
-  curl
-
   mise
 
   neovim
@@ -14,11 +11,12 @@ PACKAGES=(
 
 echo "Setup apt"
 sudo apt update -y
-sudo apt install software-properties-common
+sudo apt install -y git curl
 
 echo "Setup apt repos"
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo add-apt-repository -y ppa:jdxcode/mise
+sudo install -dm 755 /etc/apt/keyrings
+curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 
 echo "Update apt"
 sudo apt update -y
