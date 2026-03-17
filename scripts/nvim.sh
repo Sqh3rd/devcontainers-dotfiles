065952c02f42257e4dcbf4b56c7527264d05cf40
Sqh3rd <moitz417@gmail.com>
2026-03-17 20:31:32 +0100

    more sudo

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [ ! -d "~/.config/nvim" ]; then 
   echo "Cloning neovim config..."
-  git clone git@github.com:Sqh3rd/nvim-config.git "/root/.config/nvim"
+  sudo git clone git@github.com:Sqh3rd/nvim-config.git "/root/.config/nvim"
 fi
 
 echo "Installing neovim plugins..."
--- scripts/ssh_config.sh
+++ scripts/ssh_config.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
+sudo mkdir -p /root/.ssh
+sudo chmod 700 /root/.ssh
 
-if ! grep -q "github.com" ~/.ssh/known_hosts 2>/dev/null; then
-  ssh-keyscan -t ed25519 github.com >> ~/.ssh/known_hosts
+if ! grep -q "github.com" /root/.ssh/known_hosts 2>/dev/null; then
+  sudo ssh-keyscan -t ed25519 github.com >> /root/.ssh/known_hosts
 fi
