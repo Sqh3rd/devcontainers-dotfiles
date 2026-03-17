d270cd8d53d2dabb44b0384b53c6136931fa2001
Sqh3rd <moitz417@gmail.com>
2026-03-17 20:26:46 +0100

    do nvim config for sudo

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 if [ ! -d "~/.config/nvim" ]; then 
   echo "Cloning neovim config..."
-  git clone git@github.com:Sqh3rd/nvim-config.git "~/.config/nvim"
+  git clone git@github.com:Sqh3rd/nvim-config.git "/root/.config/nvim"
 fi
 
 echo "Installing neovim plugins..."
-nvim --headless "+Lazy! sync" +qa || true
+sudo nvim --headless "+Lazy! sync" +qa || true
