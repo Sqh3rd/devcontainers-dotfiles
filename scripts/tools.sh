154330b27356a781987949f9139585302e757bb1
Sqh3rd <moitz417@gmail.com>
2026-03-17 20:17:05 +0100

    redo some stuff

---
+++ scripts/tools.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PACKAGES=(
+  git
+  curl
+
+  mise
+
+  neovim
+  ripgrep
+  fzf
+)
+
+echo "Setup apt"
+sudo apt-get install software-properties-common
+
+echo "Setup apt repos"
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo add-apt-repository -y ppa:jdxcode/mise
+
+echo "Update apt"
+sudo apt update -y
+
+echo "Install Packages"
+sudo apt install -y "${PACKAGES[@]}"
+
