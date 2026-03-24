d2f50b1a29fb7a86b4c2581499c077aba4b16f1f
Sqh3rd <moitz417@gmail.com>
2026-03-24 21:07:36 +0100

    No sudo?

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 echo "Install neovim"
 curl -Lo /tmp/nvim.tar.gz \
   "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
-sudo tar -C /usr/local -xzf /tmp/nvim.tar.gz --strip-components=1
+tar -C /usr/local -xzf /tmp/nvim.tar.gz --strip-components=1
 
 if [ ! -d "~/.config/nvim" ]; then 
   echo "Cloning neovim config..."
-  sudo -E git clone git@github.com:Sqh3rd/nvim-config.git "/root/.config/nvim"
+  git clone git@github.com:Sqh3rd/nvim-config.git "/root/.config/nvim"
 fi
 
 echo "Installing neovim plugins..."
-sudo nvim --headless "+Lazy! sync" +qa || true
+nvim --headless "+Lazy! sync" +qa || true
--- scripts/tools.sh
+++ scripts/tools.sh
@@ -9,17 +9,17 @@ PACKAGES=(
 )
 
 echo "Setup apt"
-sudo apt update -y
-sudo apt install -y git curl
+apt update -y
+apt install -y git curl
 
 echo "Setup apt repos"
-sudo install -dm 755 /etc/apt/keyrings
-curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.asc 1> /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+install -dm 755 /etc/apt/keyrings
+curl -fSs https://mise.jdx.dev/gpg-key.pub | tee /etc/apt/keyrings/mise-archive-keyring.asc 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | tee /etc/apt/sources.list.d/mise.list
 
 echo "Update apt"
-sudo apt update -y
+apt update -y
 
 echo "Install Packages"
-sudo apt install -y "${PACKAGES[@]}"
+apt install -y "${PACKAGES[@]}"
 
